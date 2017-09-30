524fff3fc3e57e56527d1fe757208c58d0724aa4
liaoyf <893753543@qq.com>
2017-09-30 17:58:36 +0800

    更新说明文件

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 练习项目
 
+![demo](image/demo.png)
+
 希望模仿手机qq
--- js/components/home/Home.js
+++ js/components/home/Home.js
@@ -6,38 +6,130 @@ import {
     View,
     Text,
     StyleSheet,
-    StatusBar
+    StatusBar,
+    Image,
+    PixelRatio,
+    Button,
+    FlatList
 } from 'react-native';
 import PublicStyle from '../../css';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import LinearGradient from 'react-native-linear-gradient';
 
 class Home extends React.Component{
+    constructor(){
+        super();
+    }
+
     static navigationOptions = {
         title: '首页',
         header: null
     };
 
+    renderItem({item}){
+        return (
+            <View style={styles.itemBox}>
+                <View style={[styles.listIcon]}>
+                    <Image source={require('../../../image/vator.jpg')} style={{width: 40, height: 40, borderRadius: 40}}/>
+                </View>
+                <View style={[styles.listContent]}>
+                    <View style={styles.listContentTop}>
+                        <Text style={styles.listContentTopText}>{item.label}</Text>
+                        <View style={styles.listContentTopTime}>
+                            <Text style={styles.listContentTopTimeText}>{item.time}</Text>
+                        </View>
+                    </View>
+                    <View>
+                        <Text style={styles.listLastMessage} numberOfLines={1}>{item.lastMessage}</Text>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+
     render(){
         return (
             <View style={[PublicStyle.flex]}>
                 <StatusBar translucent={true}/>
-                <View style={[styles.header]}>
-                    <View style={[PublicStyle.flex, PublicStyle.vhCenter]}>
-                        <Text style={[styles.headerLeft]}>头像</Text>
+                <LinearGradient style={[styles.header]} colors={['#508dff', '#3ab8fe']} start={{x: 0, y: 0}} end={{x: 0.5, y: 0}}>
+                    <View style={[PublicStyle.vhCenter, styles.headerLeft]}>
+                        <Image source={require('../../../image/vator.jpg')} style={{width: 30, height: 30, borderRadius: 30}}/>
                     </View>
                     <View style={[PublicStyle.flex, PublicStyle.vhCenter]}>
                         <Text style={[styles.title]}>消息</Text>
                     </View>
-                    <View style={[PublicStyle.flex, PublicStyle.vhCenter]}>
-                        <Text style={[styles.headerRight]}>+</Text>
+                    <View style={[PublicStyle.vhCenter, styles.headerRight]}>
+                        <MaterialCommunityIcons name="plus" size={30} color="#fff"/>
+                    </View>
+                </LinearGradient>
+                <View style={[PublicStyle.flex, styles.content]}>
+                    <View style={[PublicStyle.vhCenter, styles.searchBar]}>
+                        <View style={{marginRight: 5}}>
+                            <FontAwesome name="search" color='#c7c7cc'/>
+                        </View>
+                        <View>
+                            <Text style={{color: '#a6a6a6', fontSize: 12}}>搜索</Text>
+                        </View>
+                    </View>
+                    <View style={[PublicStyle.flex]}>
+                        <FlatList
+                            data={[
+                                {
+                                    label: 'QQ看点',
+                                    lastMessage: '我们用垃圾在太平洋造了一个我们用垃圾在太平洋造了一个',
+                                    time: '下午8:43'
+                                },
+                                {
+                                    label: 'redux',
+                                    lastMessage: 'liaoyf：对于MyListItem组件来说，其onPressItem属性使用箭头函数而非bind的方式进行绑定',
+                                    time: '下午2:43'
+                                },
+                                {
+                                    label: '服务号',
+                                    lastMessage: '腾讯课堂：一份给前端老铁们的好差事，哈哈哈哈哈哈哈哈哈哈哈哈哈',
+                                    time: '上午8:23'
+                                },
+                                {
+                                    label: 'React Native社区2群',
+                                    lastMessage: '畅游网络加入了本群',
+                                    time: '下午5:54'
+                                },
+                                {
+                                    label: 'React Native社区1群',
+                                    lastMessage: '畅游网络2加入了本群',
+                                    time: '下午5:53'
+                                },
+                                {
+                                    label: 'React交流1群',
+                                    lastMessage: '畅游：这是为什么呢？',
+                                    time: '下午5:53'
+                                },
+                                {
+                                    label: '张三',
+                                    lastMessage: '张三：加油加油',
+                                    time: '下午5:53'
+                                }
+                            ]}
+                            keyExtractor={item => item.label}
+                            renderItem={this.renderItem}
+                        />
                     </View>
                 </View>
-                <View style={[styles.content]}>
-                    <Text>中间部分</Text>
-                </View>
                 <View style={[styles.footer]}>
-                    <Text>消息</Text>
-                    <Text>联系人</Text>
-                    <Text>动态</Text>
+                    <View style={[PublicStyle.flex, PublicStyle.vhCenter]}>
+                        <FontAwesome name="smile-o" color="#7f8393" size={20}/>
+                        <Text style={styles.footerText}>消息</Text>
+                    </View>
+                    <View style={[PublicStyle.flex, PublicStyle.vhCenter]}>
+                        <FontAwesome name="user-o" color="#7f8393" size={20}/>
+                        <Text style={styles.footerText}>联系人</Text>
+                    </View>
+                    <View style={[PublicStyle.flex, PublicStyle.vhCenter]}>
+                        <FontAwesome name="star-o" color="#7f8393" size={20}/>
+                        <Text style={styles.footerText}>动态</Text>
+                    </View>
                 </View>
             </View>
         );
@@ -51,15 +143,71 @@ const styles = StyleSheet.create({
         backgroundColor: '#29c1fe'
     },
     headerLeft: {
-
+        width: 60
     },
-    title: {},
-    headerRight: {},
+    title: {
+        color: '#fff',
+        fontSize: 18
+    },
+    headerRight: {
+        width: 40
+    },
     content: {
-        flex: 1
+        backgroundColor: '#fff'
     },
     footer: {
-        height: 60
+        height: 40,
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        borderTopWidth: 1 / PixelRatio.get(),
+        borderTopColor: '#ccc'
+    },
+    footerText: {
+        fontSize: 10
+    },
+    addIcon: {
+        color: '#fff',
+        fontSize: 40
+    },
+    searchBar: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        height: 30,
+        backgroundColor: '#eeeff3',
+        borderRadius: 3,
+        margin: 8
+    },
+    itemBox: {
+        flexDirection: 'row'
+    },
+    listIcon: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    listContent: {
+        flex: 1,
+        paddingRight: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderBottomWidth: 1 / PixelRatio.get(),
+        borderBottomColor: '#ccc'
+    },
+    listContentTop: {
+        flexDirection: 'row'
+    },
+    listContentTopText: {
+        flex: 1,
+        color: '#000',
+        fontSize: 18
+    },
+    listContentTopTimeText: {
+        color: '#999',
+        fontSize: 12
+    },
+    listLastMessage: {
+        fontSize: 14
     }
 });
 
